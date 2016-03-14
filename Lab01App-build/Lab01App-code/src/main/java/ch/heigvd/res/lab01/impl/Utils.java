8d37bbebcea1ffebe5f609aa8b824e7f1039c7f2
CotzaDev <andrea@cotza.me>
2016-03-15 00:38:11 +0100

    Done Utils class

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/Utils.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/Utils.java
@@ -20,7 +20,31 @@ public class Utils {
    * contain any line separator, then the first element is an empty string.
    */
   public static String[] getNextLine(String lines) {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+
+    String[] result = new String[2];
+    Boolean endWithNL = false;
+
+    // Check if there is a new line at the end
+    if (lines.charAt(lines.length() - 1) == '\n' || lines.charAt(lines.length() - 1) == '\r'){
+      endWithNL = true;
+    }
+
+    // '\R' matches @CRLF, @CR or @LF only. (\r\n, \r or \n)
+    // '?<=' to include the delimiter char in the part before the split
+    String[] splitedLines = lines.split("(?<=\\R)");
+
+    // Check if there is only one line not ending with a new line
+    if (splitedLines.length == 1 && !endWithNL) {
+      result[0] = "";
+      result[1] = lines;
+    }
+    else {
+      result[0] = splitedLines[0];
+      // We take only what comes after the first line
+      result[1] = lines.substring(splitedLines[0].length());
+    }
+
+    return result;
   }
 
 }
